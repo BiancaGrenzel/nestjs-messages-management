8922b369bd79ea7f5abb23adb289526a556447d6
Bianca Grenzel Severo <biancagrenzel@gmail.com>
2022-11-20 21:14:57 -0300

    feat: add messages update endpoint

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -24,4 +24,14 @@ export class MessagesService {
   create(message: Message) {
     return this.messages.push(message);
   }
+
+  update(id: number, message: Message) {
+    const index = this.messages.findIndex(
+      (message: Message) => message.id === id,
+    );
+
+    this.messages[index] = message;
+
+    return message;
+  }
 }
